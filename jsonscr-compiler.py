84e2e6f087be3ec4e79e3f55449ef8f6ae1b868d
Blizzardfur <111100393+Blizzardfur-Maxxx@users.noreply.github.com>
2024-03-21 10:24:46 -0500

    strings

--- jsonscr-compiler.py
+++ jsonscr-compiler.py
@@ -17,12 +17,16 @@ class ScriptRunner:
         code += "\n"
 
         for variable in json_data.get("Variables", []):
-            code += f"{variable['name']} = {variable['value']}\n"
+            var_name = variable["name"]
+            var_value = variable["value"]
+            var_type = variable["type"]
+            if var_type == "str":
+                var_value = f'"{var_value}"'
+            code += f"{var_name} = {var_value}\n"
         code += "\n"
 
         class_name = json_data["Class"]["name"]
         code += f"class {class_name}:\n"
-
         for func_name, func_details in json_data["Class"]["functions"].items():
             code += f"    def {func_name}(self"
             if "parameters" in func_details:
@@ -33,14 +37,12 @@ class ScriptRunner:
                         param_str += f"={default_value}"
                     code += f", {param_str}"
             code += "):\n"
-
             implementation = func_details.get("implementation", "")
             implementation_lines = implementation.split("\n")
             for line in implementation_lines:
                 code += f"        {line}\n"
 
         if "main" in json_data["Class"]["functions"]:
-            main_func_impl = json_data["Class"]["functions"]["main"]["implementation"]
             code += f"\nif __name__ == '__main__':\n"
             code += f"    {class_name}().main()\n"
 
